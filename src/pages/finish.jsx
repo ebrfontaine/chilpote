403ca83619a009fddb85a20dabc58384448242f9
Elliott Fontaine <ebrfontaine@gmail.com>
2023-08-17 13:28:06 -0400

    All

---
+++ src/pages/finish.jsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Finish = () => {
+    const redeem = async (e) => {
+        document.getElementById("card").style.opacity = "0"
+        document.getElementById("text").style.opacity = "100"
+        document.getElementById("free").style.opacity = "100"
+        e.target.style.opacity = "0"
+        e.target.style.cursor = "default"
+    }
+    return (
+        <div>
+
+            <div className='flex w-full h-full justify-center relative mt-[2rem]'>
+                <Image className='' width={200} height={200} alt={"cilpote"} src={"/cmg-medallion-logo.svg"}></Image>
+            </div>
+            <h1 className='mt-5 left-1/2 -translate-x-1/2 absolute text-xl font-bold'>Happy Birthday</h1>
+            <h1 className='mt-[4rem] left-1/2  underline -translate-x-1/2 absolute text-4xl font-black'>QUINN!!!</h1>
+            <div className='flex justify-center'>
+                <div  className='top-[50%] -translate-y-1/2 absolute w-[312px] h-[194px] rounded-xl bg-[#e9eaea] animate-bounce'>
+                    <h1 id="text" className='left-[10%] absolute top-1/2 -translate-y-1/2 text-2xl font-bold opacity-0 text-black'>098fd-suio-hdsu-9d32</h1>
+                    <Image className='opacity-100 absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2' value="poop" id="card" width={200} height={100} alt={"cilpote"} src={"/chipotle.png"}></Image>
+                </div>
+            </div>
+            <h1 className='top-[77%] left-1/2 -translate-x-1/2 absolute text-2xl font-bold opacity-0 text-center' id="free">$20 Chipotle gift card <br></br> + <br></br> one time free pickup</h1>
+            <button className=' px-10 py-2 rounded-full text-[#220a01] font-bold text-l bg-white top-[85%] left-1/2 -translate-x-1/2 absolute' onClick={redeem}>REDEEM NOW!</button>
+            
+            
+        </div>
+    )
+
+    
+}
+export default Finish
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,11 +5,47 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const activate = "aouh4hsdao19m2a0" 
+  const submitContact = async (event) => {
+    
+    let inp = ""
+    inp+=event.target.a.value
+    inp+=event.target.b.value
+    inp+=event.target.c.value
+    inp+=event.target.d.value
+    if (inp == activate) {
+
+    } else {
+    //   event.preventDefault();
+    // alert("Incorrect");
+    }
+    
+  };
 
   return (
     <div>
-      <Link href={"/drivers"}><h1 className='underline'>View Drivers</h1></Link>
+      <div className='flex w-full h-full justify-center mt-[2rem]'>
+        <Image className='' width={200} height={200} alt={"cilpote"} src={"/cmg-medallion-logo.svg"}></Image>
+        
+      </div>
+      <h1 className='text-center mt-[9rem] font-bold text-xl '>Activation Code</h1>
+      <form className='justify-center flex mt-[2rem]' id="verify" onSubmit={submitContact} action='/finish' method='post'>
+        <h1 className='text-3xl h-[3rem] ml-[10rem] absolute w-3'>-</h1>
+        <h1 className='text-3xl h-[3rem] mr-[10rem] absolute w-3'>-</h1>
+        <h1 className='text-3xl h-[3rem] absolute w-3'>-</h1>
+        <input className='text-white focus:opacity-100 text-lg font-bold text-center rounded-lg absolute w-[4rem] h-[3rem] mr-[15rem] bg-slate-700 opacity-50 border-slate-900 border-b-4'  type="text" name="a" minLength={4} maxLength={4} required id="a" />
+        <input className='text-white text-lg font-bold focus:opacity-100 text-center rounded-lg absolute w-[4rem] h-[3rem] mr-[5rem] bg-slate-700 opacity-50 border-slate-900 border-b-4' type="text" name="b" minLength={4} maxLength={4} required id="b" />
+        <input className='text-white text-lg font-bold focus:opacity-100 text-center rounded-lg w-[4rem] h-[3rem] absolute ml-[5rem] bg-slate-700 opacity-50 border-slate-900 border-b-4' type="text" name="c" minLength={4} maxLength={4} required id="c" />
+        
+        
+        <input className='text-white text-lg font-bold focus:opacity-100 text-center rounded-lg w-[4rem] h-[3rem] absolute ml-[15rem] bg-slate-700 opacity-50 border-slate-900 border-b-4' type="text" name="d" minLength={4} maxLength={4} required id="d" />
+        
+        
       
+        <button className='mt-[10rem] rounded-lg bg-white text-black h-[2.5rem] w-[8rem] opacity-50 focus:opacity-100'  id="sub" type="submit">Submit</button>
+      </form>
+        
+
     </div>
   )
 }
